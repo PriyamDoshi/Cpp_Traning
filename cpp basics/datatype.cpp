62c563ea1fe04d5362e3f7f50f4c922cc2385c8c
PriyamDoshi <124236811+PriyamDoshi@users.noreply.github.com>
2023-04-04 15:35:33 +0530

    Add files via upload

---
+++ cpp basics/datatype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a = 10;
+    cout<< "sise of int "<< sizeof(a)<<endl;
+    float b = 10;
+    cout<< "sise of float "<< sizeof(b)<<endl;
+    char c = 'A';
+   
+    cout<<"cagr in c is "<<c<<endl;
+    cout<< "sise of char "<< sizeof(c)<<endl;
+    
+
+
+    return 0;
+}
